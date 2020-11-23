2cfa4e55851976049d9af26c8d39dd4a07d4c575
sebastientromp <sebastien.tromp@gmail.com>
2020-11-23 09:54:42 +0100

    add vpc config mysql.end() and set reasonable timeout

--- src/input.ts
+++ src/input.ts
@@ -1,3 +1,5 @@
+import { BoosterType, RewardType } from '@firestone-hs/reference-data';
+
 export interface Input {
 	readonly type: 'duels' | 'paid-duels';
 	readonly reviewId: string;
@@ -10,6 +12,7 @@ export interface Input {
 	readonly chosenLootIndex: number;
 	readonly treasureOptions: readonly string[];
 	readonly chosenTreasureIndex: number;
+	readonly rewards: DuelsRewardsInfo;
 	readonly currentWins: number;
 	readonly currentLosses: number;
 	readonly rating: number;
@@ -19,4 +22,14 @@ export interface Input {
 export interface OptionBundle {
 	readonly bundleId: string;
 	readonly elements: readonly string[];
+}
+
+export interface DuelsRewardsInfo {
+	readonly Rewards: readonly DuelsRewardInfo[];
+}
+
+export interface DuelsRewardInfo {
+	readonly Type: RewardType;
+	readonly Amount: number;
+	readonly BoosterId: BoosterType;
 }
