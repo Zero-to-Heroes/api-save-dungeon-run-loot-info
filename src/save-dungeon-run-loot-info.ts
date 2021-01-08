da176a9a2113a7e0d689a4d20e1aec690a467e1e
sebastientromp <sebastien.tromp@gmail.com>
2021-01-08 16:04:34 +0100

    escape input

--- src/save-dungeon-run-loot-info.ts
+++ src/save-dungeon-run-loot-info.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
+import SqlString from 'sqlstring';
 import { Input } from './input';
 import { getConnection } from './services/rds';
 import { formatDate } from './services/utils';
@@ -74,8 +75,22 @@ export default async (event): Promise<any> => {
 };
 
 const saveStartingHeroPower = async (input: Input, creationDate: string, mysqlBgs): Promise<void> => {
+	const escape = SqlString.escape;
 	if (input.startingHeroPower) {
-		const values = `'${input.type}', '${creationDate}', '${input.reviewId}', '${input.runId}', '${input.userId}', '${input.userName}', 'hero-power', '${input.startingHeroPower}', 1, ${input.currentWins}, ${input.currentLosses}, ${input.rating}`;
+		const values = `
+			${escape(input.type)}, 
+			${escape(creationDate)}, 
+			${escape(input.reviewId)}, 
+			${escape(input.runId)}, 
+			${escape(input.userId)}, 
+			${escape(input.userName)}, 
+			'hero-power', 
+			${escape(input.startingHeroPower)}, 
+			1, 
+			${escape(input.currentWins)}, 
+			${escape(input.currentLosses)}, 
+			${escape(input.rating)}
+		`;
 		// https://stackoverflow.com/questions/1361340/how-to-insert-if-not-exists-in-mysql
 		// Only insert the starting hero power once
 		const query = `
@@ -84,8 +99,8 @@ const saveStartingHeroPower = async (input: Input, creationDate: string, mysqlBg
 			SELECT ${values}
 			WHERE NOT EXISTS (
 				SELECT * FROM dungeon_run_loot_info 
-				WHERE adventureType = '${input.type}'
-				AND runId = '${input.runId}'
+				WHERE adventureType = ${escape(input.type)}
+				AND runId = ${escape(input.runId)}
 				AND bundleType = 'hero-power'
 				LIMIT 1
 			)				
@@ -97,8 +112,22 @@ const saveStartingHeroPower = async (input: Input, creationDate: string, mysqlBg
 };
 
 const saveSignatureTreasure = async (input: Input, creationDate: string, mysqlBgs): Promise<void> => {
+	const escape = SqlString.escape;
 	if (input.signatureTreasure) {
-		const values = `'${input.type}', '${creationDate}', '${input.reviewId}', '${input.runId}', '${input.userId}', '${input.userName}', 'signature-treasure', '${input.signatureTreasure}', 1, ${input.currentWins}, ${input.currentLosses}, ${input.rating}`;
+		const values = `
+			${escape(input.type)}, 
+			${escape(creationDate)}, 
+			${escape(input.reviewId)}, 
+			${escape(input.runId)}, 
+			${escape(input.userId)}, 
+			${escape(input.userName)}, 
+			'signature-treasure', 
+			${escape(input.signatureTreasure)}, 
+			1, 
+			${escape(input.currentWins)}, 
+			${escape(input.currentLosses)}, 
+			${escape(input.rating)}
+		`;
 		// https://stackoverflow.com/questions/1361340/how-to-insert-if-not-exists-in-mysql
 		// Only insert the starting hero power once
 		const query = `
@@ -107,8 +136,8 @@ const saveSignatureTreasure = async (input: Input, creationDate: string, mysqlBg
 			SELECT ${values}
 			WHERE NOT EXISTS (
 				SELECT * FROM dungeon_run_loot_info 
-				WHERE adventureType = '${input.type}'
-				AND runId = '${input.runId}'
+				WHERE adventureType = ${escape(input.type)}
+				AND runId = ${escape(input.runId)}
 				AND bundleType = 'signature-treasure'
 				LIMIT 1
 			)				
@@ -119,27 +148,38 @@ const saveSignatureTreasure = async (input: Input, creationDate: string, mysqlBg
 };
 
 const saveLootBundles = async (input: Input, creationDate: string, mysqlBgs): Promise<void> => {
+	const escape = SqlString.escape;
 	if (input.lootBundles?.length) {
-		const lootValues = `'${input.type}', '${creationDate}', '${input.reviewId}', '${input.runId}', '${
-			input.userId
-		}', '${input.userName}', 'loot', '${input.lootBundles[0].bundleId}', '${input.lootBundles[0].elements.join(
-			',',
-		)}', '${input.lootBundles[1].bundleId}', '${input.lootBundles[1].elements.join(',')}', '${
-			input.lootBundles[2].bundleId
-		}', '${input.lootBundles[2].elements.join(',')}', ${input.chosenLootIndex}, ${input.currentWins}, ${
-			input.currentLosses
-		}, ${input.rating}`;
+		const lootValues = `
+			${escape(input.type)}, 
+			${escape(creationDate)}, 
+			${escape(input.reviewId)}, 
+			${escape(input.runId)}, 
+			${escape(input.userId)}, 
+			${escape(input.userName)}, 
+			'loot', 
+			${escape(input.lootBundles[0].bundleId)}, 
+			${escape(input.lootBundles[0].elements.join(','))}, 
+			${escape(input.lootBundles[1].bundleId)}, 
+			${escape(input.lootBundles[1].elements.join(','))}, 
+			${escape(input.lootBundles[2].bundleId)}, 
+			${escape(input.lootBundles[2].elements.join(','))}, 
+			${escape(input.chosenLootIndex)}, 
+			${escape(input.currentWins)}, 
+			${escape(input.currentLosses)}, 
+			${escape(input.rating)}
+		`;
 		const query = `
 			INSERT INTO dungeon_run_loot_info
 			(adventureType, creationDate, reviewId, runId, userId, userName, bundleType, option1, option1Contents, option2, option2Contents, option3, option3Contents, chosenOptionIndex, wins, losses, rating)
 			SELECT ${lootValues}
 			WHERE NOT EXISTS (
 				SELECT * FROM dungeon_run_loot_info 
-				WHERE adventureType = '${input.type}'
-				AND runId = '${input.runId}'
+				WHERE adventureType = ${escape(input.type)}
+				AND runId = ${escape(input.runId)}
 				AND bundleType = 'loot'
-				AND wins = ${input.currentWins}
-				AND losses = ${input.currentLosses}
+				AND wins = ${escape(input.currentWins)}
+				AND losses = ${escape(input.currentLosses)}
 				LIMIT 1
 			)
 			
@@ -150,19 +190,35 @@ const saveLootBundles = async (input: Input, creationDate: string, mysqlBgs): Pr
 };
 
 const saveTreasures = async (input: Input, creationDate: string, mysqlBgs): Promise<void> => {
+	const escape = SqlString.escape;
 	if (input.treasureOptions?.length) {
-		const lootValues = `'${input.type}', '${creationDate}', '${input.reviewId}', '${input.runId}', '${input.userId}', '${input.userName}', 'treasure', '${input.treasureOptions[0]}', '${input.treasureOptions[1]}', '${input.treasureOptions[2]}', ${input.chosenTreasureIndex}, ${input.currentWins}, ${input.currentLosses}, ${input.rating}`;
+		const lootValues = `
+			${escape(input.type)}, 
+			${escape(creationDate)}, 
+			${escape(input.reviewId)}, 
+			${escape(input.runId)}, 
+			${escape(input.userId)}, 
+			${escape(input.userName)}, 
+			'treasure', 
+			${escape(input.treasureOptions[0])}, 
+			${escape(input.treasureOptions[1])}, 
+			${escape(input.treasureOptions[2])}, 
+			${escape(input.chosenTreasureIndex)}, 
+			${escape(input.currentWins)}, 
+			${escape(input.currentLosses)}, 
+			${escape(input.rating)}
+		`;
 		const query = `
 			INSERT INTO dungeon_run_loot_info
 			(adventureType, creationDate, reviewId, runId, userId, userName, bundleType, option1, option2, option3, chosenOptionIndex, wins, losses, rating)
 			SELECT ${lootValues}
 			WHERE NOT EXISTS (
 				SELECT * FROM dungeon_run_loot_info 
-				WHERE adventureType = '${input.type}'
-				AND runId = '${input.runId}'
+				WHERE adventureType = ${escape(input.type)}
+				AND runId = ${escape(input.runId)}
 				AND bundleType = 'treasure'
-				AND wins = ${input.currentWins}
-				AND losses = ${input.currentLosses}
+				AND wins = ${escape(input.currentWins)}
+				AND losses = ${escape(input.currentLosses)}
 				LIMIT 1
 			)
 		`;
@@ -172,11 +228,25 @@ const saveTreasures = async (input: Input, creationDate: string, mysqlBgs): Prom
 };
 
 const saveRewards = async (input: Input, creationDate: string, mysqlBgs): Promise<void> => {
+	const escape = SqlString.escape;
 	if (input.rewards?.Rewards?.length) {
-		console.log('rewards', input.rewards, input);
+		// console.log('rewards', input.rewards, input);
 		const values = input.rewards.Rewards.map(
 			reward =>
-				`('${input.type}', '${creationDate}', '${input.reviewId}', '${input.runId}', '${input.userId}', '${input.userName}', ${reward.Type}, ${reward.Amount}, ${reward.BoosterId}, ${input.currentWins}, ${input.currentLosses}, ${input.rating})`,
+				`(
+					${escape(input.type)}, 
+					${escape(creationDate)}, 
+					${escape(input.reviewId)}, 
+					${escape(input.runId)}, 
+					${escape(input.userId)}, 
+					${escape(input.userName)}, 
+					${escape(reward.Type)}, 
+					${escape(reward.Amount)}, 
+					${escape(reward.BoosterId)}, 
+					${escape(input.currentWins)}, 
+					${escape(input.currentLosses)}, 
+					${escape(input.rating)}
+				)`,
 		).join('\n,');
 
 		const query = `
@@ -184,7 +254,6 @@ const saveRewards = async (input: Input, creationDate: string, mysqlBgs): Promis
 			(adventureType, creationDate, reviewId, runId, userId, userName, rewardType, rewardAmount, rewardBoosterId, wins, losses, rating)
 			VALUES ${values}
 		`;
-		console.log('running query', query);
 		await mysqlBgs.query(query);
 	}
 };
